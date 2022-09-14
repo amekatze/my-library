282f7d40e42af971904ee682ef0b1111ece45a00
amekatze <setharie@gmail.com>
2022-09-15 05:49:56 +0900

    Add delete button

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
+
 let library = [
    {
         title: "The Picture of Dorian Gray",
         author: "Oscar Wilde",
-        pages: 192
+        pages: 192,
+        read: true,
+        id: 1663185789550
     },
     {
         title: "Awareness",
         author: "Anthony De Mello",
-        pages: 184
+        pages: 184,
+        read: true,
+        id: 1663185805860
+    },
+    {
+        title: "The Laws of Human Nature",
+        author: "Robert Greene",
+        pages: 624,
+        read: true,
+        id: 1663185818993
     }
 ];
 
+const table = document.getElementById("book-table");
+library.forEach(book => { addToBookTable(book);})
 
-function Book(title, author, pages){
+function addToBookTable(book) {
+    let row = table.insertRow();
+    let title = row.insertCell(0);
+    title.innerHTML = book.title;
+    let author = row.insertCell(1);
+    author.innerHTML = book.author;
+    let pages = row.insertCell(2);
+    pages.innerHTML = book.pages;
+    let read = row.insertCell(3);
+    read.innerHTML = book.read;
+    let del = row.insertCell(4);
+    del.innerHTML = `<button class='del' id='${book.id}'>DELETE</button>`;
+}
+
+function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
+    this.id = Date.now();
 }
 
 Book.prototype.info = function(){
@@ -23,21 +53,27 @@ Book.prototype.info = function(){
 }
 
 function addBookToLibrary(book) {
-    document.getElementById("test").innerHTML += "</br>" + Object.values(book);
     library.push(book);
+    addToBookTable(book);
 }
-
-library.forEach(book => document.getElementById("test").innerHTML += "</br>" + Object.values(book))
 
 
 const addBookButton = document.getElementById("addnewbook");
 
-
 function addBook() {
-    console.log("button works")
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    const book = new Book(title, author, pages);
+    let read = document.getElementById("read").checked;
+    const book = new Book(title, author, pages, read);
+    console.log(read)
     addBookToLibrary(book);
 }
+
+let del = document.querySelectorAll('.del');
+del.forEach(btn => btn.onclick = function(){
+    const index = library.findIndex((book) => book.id == this.id);
+    library = library.slice(index)
+    const row = this.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+} )
