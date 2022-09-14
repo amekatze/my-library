dc345319c30304f65290fa25f8e2b81a33509693
amekatze <setharie@gmail.com>
2022-09-14 23:11:00 +0900

    Add book function and display of library in the dom

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let myLibrary = [];
+let library = [
+   {
+        title: "The Picture of Dorian Gray",
+        author: "Oscar Wilde",
+        pages: 192
+    },
+    {
+        title: "Awareness",
+        author: "Anthony De Mello",
+        pages: 184
+    }
+];
 
 
 function Book(title, author, pages){
@@ -11,7 +22,22 @@ Book.prototype.info = function(){
     return (this.title + ", " + this.author + ", " + this.pages)
 }
 
+function addBookToLibrary(book) {
+    document.getElementById("test").innerHTML += "</br>" + Object.values(book);
+    library.push(book);
+}
 
-function addBookToLibrary() {
-    
+library.forEach(book => document.getElementById("test").innerHTML += "</br>" + Object.values(book))
+
+
+const addBookButton = document.getElementById("addnewbook");
+
+
+function addBook() {
+    console.log("button works")
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    const book = new Book(title, author, pages);
+    addBookToLibrary(book);
 }
