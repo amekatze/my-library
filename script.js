912cd3e0be6e31681bdbafb798e8ffdb25b28bac
amekatze <setharie@gmail.com>
2022-10-05 08:17:15 +0900

    Refactor code to class

--- script.js
+++ script.js
@@ -27,13 +27,15 @@ let library = [
     }
 ];
 
-// book constructor
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = Date.now();
+// book class
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = Date.now();  
+    }  
 }
 
 //iterate through existing library
